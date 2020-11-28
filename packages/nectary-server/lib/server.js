8a6b8310caf34fe2dee0123e074407b1ab4d144f
mingdev <mingdev@163.com>
2020-11-28 17:44:46 +0800

    代码重构

--- packages/nectary-server/lib/server.js
+++ packages/nectary-server/lib/server.js
@@ -1,45 +1,18 @@
 const connect = require('connect');
-const connectRoute = require('connect-route');
 
-class Server {
+module.exports = class Server {
   constructor(nectary) {
     this.nectary = nectary;
     this.options = nectary.options;
     const app = connect();
     this.app = app;
     this.render = app;
-    nectary.hook('builder:useMiddleware', (middleware) => {
-      if (middleware) app.use(middleware);
-    });
-    nectary.hook('builder:entryList', (entryList = []) => {
-      if (entryList.length) this.setEntryToRouter(entryList);
-    });
+
+    // devMiddleware placeholder
+    if (this.options.dev) {
+      nectary.hook('server:devMiddleware', (devMiddleware)=>{
+        app.use(devMiddleware)
+      })
+    }
   }
-
-  setEntryToRouter(entryList) {
-    const isEmptyObject = obj => !!Object.keys(obj).length;
-    const isFunction = fn => Object.prototype.toString.call(fn) === '[object Function]';
-
-    entryList.forEach(entry => {
-      const {url, appServerSideProps, Component, getServerSideProps, pageScripts, pageStyles, renderToString} = entry;
-      this.app.use(connectRoute(router => {
-        router.get(url, async (req, res) => {
-          let appStore = {};
-          let pageStore = {};
-          if (isFunction(appServerSideProps)) appStore = await appServerSideProps({req, res});
-          if (isFunction(getServerSideProps)) pageStore = await getServerSideProps({req, res});
-          const store = {...appStore || {}, ...pageStore || {}};
-          const content = await renderToString({
-            Component,
-            store: isEmptyObject(store) ? null : store,
-            pageScripts,
-            pageStyles
-          });
-          res.send(content);
-        })
-      }))
-    })
-  }
-}
-
-module.exports = Server;
+};
