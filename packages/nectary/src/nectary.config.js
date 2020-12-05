778ff7c80d4aa80543a87a09ea97a8c17f96b8f4
mingdev <mingdev@163.com>
2020-12-05 16:51:19 +0800

    增加配置

--- packages/nectary/src/nectary.config.js
+++ packages/nectary/src/nectary.config.js
@@ -18,6 +18,23 @@ export default {
     dir: {
       server: 'views',
       static: 'static'
+    },
+    filenames: {
+      // { isDev, isClient, isServer }
+      app: ({isDev}) => isDev ? '[name].js' : 'js/[contenthash:8].js',
+      chunk: ({isDev}) => isDev ? '[name].js' : 'js/[contenthash:8].js',
+      css: ({isDev}) => isDev ? '[name].css' : 'css/[contenthash:8].css',
+      img: ({isDev}) => isDev ? '[path][name].[ext]' : 'images/[contenthash:8].[ext]',
+      font: ({isDev}) => isDev ? '[path][name].[ext]' : 'fonts/[contenthash:8].[ext]',
+      video: ({isDev}) => isDev ? '[path][name].[ext]' : 'videos/[contenthash:8].[ext]'
+    }
+  },
+  server: {
+    compressor: {
+      threshold: 0
+    },
+    etag: {
+      weak: false
     }
   },
   eslint: true,
